1911986bb8b9eeeaa0874ec30c63759f07762328
mishaamir <mishaamir357@gmail.com>
2026-09-11 19:43:47 -0700

    Assignment 4

--- Assignment 4/Assignment 3/script.js
+++
@@ -1,9 +0,0 @@
-let heading = document.getElementById("title");
-let paragraph = document.getElementById("message");
-
-document.getElementById("showBtn").onclick = function() {
-    alert(heading.innerText);
-    console.log(paragraph.innerText);
-};
-
-document.write("<p>Welcome! This message is displayed using document.write().</p>");
